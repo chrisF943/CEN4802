a99c599358d1ae5863d97de848856cdebb3fe6f4
Christopher Faris <142948338+chrisF943@users.noreply.github.com>
2024-08-19 19:39:56 -0400

    removed docs

--- src/assignment1.java
+++ src/assignment1.java
@@ -3,12 +3,7 @@ public class assignment1 {
         System.out.println(fibonacciString(10));
     }// end of main
 
-    /**
-     * Calculates the nth number in the Fibonacci sequence.
-     *
-     * @param n the position of the number in the Fibonacci sequence
-     * @return the nth number in the Fibonacci sequence
-     */
+
     public static int fibonacci(int n) {
         if (n == 0) {
             return 0;
@@ -19,12 +14,7 @@ public class assignment1 {
         return (fibonacci(n - 1) + fibonacci(n - 2));
     }// end of fibonacci
 
-    /**
-     * Returns a string describing the nth Fibonacci number.
-     *
-     * @param n the position of the number in the Fibonacci sequence
-     * @return a string containing the position and value of the nth Fibonacci number
-     */
+
     public static String fibonacciString(int n) {
         return "The " + n + "th number in the  Fibonacci sequence is " + fibonacci(n);
     }// end of fibonacciString
